14546c153a32f1341052bc9d6cfeb3fd487a26a2
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-03-20 16:23:16 +0000

    modules defined

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-:)) thguis us a uhrwauihg
+//Module for the data manipulation 
+
+var budgetController = (function() {
+
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
+
+})();
+
+//Module for changing the user interface
+
+var UIController = (function() {
+
+    //code
+
+}) ();
+
+//module for changing the behaviour
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
+}) (budgetController, UIController);
