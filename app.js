c336b36a517cc47a701dc3b11ff5e96b46d3c264
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-03-20 17:08:22 +0000

    added event listeners

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-//Module for the data manipulation 
+//Module for the data manipulating teh budget
 
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+//cpde
 
 })();
 
@@ -24,16 +14,41 @@ var UIController = (function() {
 
 }) ();
 
-//module for changing the behaviour
+
+//module for changing the behaviour (app controller)
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
+         // 1. Get the field input data
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        //2. add item to the budget controller 
+
+        //33. Add the new item to the UI
+
+        //4 . Calculate the budget
+
+        //5 Display the budget on the UI
+        console.log('hi')
     }
 
+    //for mouse press
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    //for enter key
+    document.addEventListener('keypress', function (event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+
+        }
+
+    });
+
 }) (budgetController, UIController);
+
+
+
+
+
+
