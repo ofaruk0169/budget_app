06a3526254b32347be712f90553b38f732c7266c
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-03-21 12:09:23 +0000

    can display the values in the DOM

--- app.js
+++ app.js
@@ -75,7 +75,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputButton: '.add__btn'
+        inputButton: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list',
     }
 
     return {
@@ -92,13 +94,27 @@ var UIController = (function() {
         },
 
         addListItem: function(obj, type) {
+            
+            var html, newHtml, element;
+            // Create HTML string with placeholder text. The % sign is what you want to replace. 
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-0"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div> </div></div>';
 
-            // Create HTML string with placeholder text
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 
-            //REPLACE THJE PLACEHOLDER TEXT WITH SOME ACTUAL DATA 
+            }
 
-            //INSERT the html into the DOM
+            //REPLACE THJE PLACEHOLDER TEXT WITH SOME ACTUAL DATA. The % followed by the data you would like to insert
 
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            //INSERT the html into the DOM (insert adjacenthtml method, this places our new html after the relevent html element) 
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
 
         },
 
@@ -152,7 +168,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         //2. add item to the budget controller 
         var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //33. Add the new item to the UI
-
+        UICtrl.addListItem(newItem, input.type);
         //4 . Calculate the budget
 
         //5 Display the budget on the UI
