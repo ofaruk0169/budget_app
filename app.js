d645494172b1b788884853dda9da954509bbad39
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-03-21 10:20:12 +0000

    items are now added to the object holdinhg information about expenses and incomes

--- app.js
+++ app.js
@@ -1,10 +1,71 @@
-//Module for the data manipulating teh budget
+//Module for the data manipulating the budget
 
 var budgetController = (function() {
 
-//cpde
+    //The contructor function is allowing new instances of similar objects
+
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+//These data objects are allowing us to store the information gained from the previous object ^
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem;
+
+            //This is the data type in the allItems object. Followed by the length of that array minus 1. Then grab the last id but add 1
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+            } else {
+                ID = 0;
+            }
+            //depending on the type, the object corresponding with the type will be created ^
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            //The "type" here will add to the specified type in the function parameter. This new object we have created is then pushed 
+            //to the corresponding array
+            data.allItems[type].push(newItem);
+
+            //return the new element 
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
+
+
 
 })();
+
+
+
+
 
 //Module for changing the user interface
 
@@ -21,11 +82,24 @@ var UIController = (function() {
         getInput: function() {
 
             return {
+
+                //This returns the information we need as a key value pair type of thing
                 type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
            
+        },
+
+        addListItem: function(obj, type) {
+
+            // Create HTML string with placeholder text
+
+            //REPLACE THJE PLACEHOLDER TEXT WITH SOME ACTUAL DATA 
+
+            //INSERT the html into the DOM
+
+
         },
 
         getDOMstrings: function() {
@@ -36,41 +110,68 @@ var UIController = (function() {
 }) ();
 
 
+
+
+
+
+
+
+
+
 //module for changing the behaviour (app controller)
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+        var setupEventListeners = function() {
+
+        var DOM = UICtrl.getDOMstrings();
+
+        //for mouse press
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+        //for enter key
+        document.addEventListener('keypress', function (event) {
+
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+
+            }
+
+        });
+
+    };
+    
+   
 
     var ctrlAddItem = function() {
+        var input, newItem;
+
          // 1. Get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
+        
         //2. add item to the budget controller 
-
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //33. Add the new item to the UI
 
         //4 . Calculate the budget
 
         //5 Display the budget on the UI
         
-    }
+    };
 
-    //for mouse press
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
-    //for enter key
-    document.addEventListener('keypress', function (event) {
-
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
+    };
 
-    });
+
 
 }) (budgetController, UIController);
 
+
+controller.init();
 
 
 
